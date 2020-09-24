260ef28ccb5ee2ab8e4b36f510e426f3a97ab6c3
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-09-23 21:22:15 -0300

    ⚡  chore: add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+
+import Head from 'next/head';
 import { AppProps } from 'next/app';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+import GlobalStyles from '../styles/global';
+import { PaletteProvider } from '../hooks/palette';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>Tons</title>
+      </Head>
+
+      <PaletteProvider>
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </PaletteProvider>
+    </>
+  );
+};
 
 export default MyApp;
